af263f5348e4047c4e9558760185e9d3280fed99
FiV0 <finn.volkel@gmail.com>
2021-07-17 15:10:03 +0200

    Move java to different directory, expand build.clj.

--- build.clj
+++ build.clj
@@ -1,18 +1,56 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]))
+  (:require [clojure.java.io :as io]
+            [clojure.tools.build.api :as b]))
 
 (def lib 'fiv0/tools.build-testing)
 (def version (format "1.2.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
-(def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
 
-(defn clean [_]
+(defn clean
+  "Cleans the target path."
+  [_]
   (b/delete {:path "target"}))
 
-(defn java [_]
-  (b/javac {:src-dirs ["src"]
-            :class-dir "target/classes"}))
+(defn java
+  "Compiles the java classes under `src/java`."
+  [_]
+  (b/javac {:src-dirs ["src/java"]
+            :class-dir "target/classes"
+            :basis basis}))
+
+(defn java-pom
+  "Creates a pom.xml file at the root of the repository for the java files.
+  Useful when working on the java files with other tools integrating maven."
+  [_]
+  (let [pom-dir (io/file class-dir "META-INF/maven/" (namespace lib) (name lib))
+        pom-xml (io/file pom-dir "pom.xml")
+        pom-properties (io/file pom-dir "pom.properties")]
+    (b/write-pom {:class-dir class-dir
+                  :lib lib
+                  :version version
+                  :basis basis
+                  :src-dirs ["src/java"]})
+    (io/copy pom-xml (io/file "pom.xml"))
+    (io/delete-file pom-xml)
+    (io/delete-file pom-properties)
+    nil))
+
+(defn jar
+  "Creates a jar file of the project."
+  [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src/clj"]})
+  (b/copy-dir {:src-dirs ["src/clj" "resources"]
+               :target-dir class-dir})
+  (java nil)
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
 
 (comment
+  (java-pom nil)
   (java nil))
--- src/java/io/dummy/Foo.java
+++ src/java/io/dummy/Foo.java
@@ -1,10 +1,15 @@
 package io.dummy;
 
+import it.unimi.dsi.bits.BitVector;
+import it.unimi.dsi.bits.BitVectors;
+
 public class Foo {
     private int data;
+    BitVector bv;
 
     public Foo(int data){
         this.data = data;
+        this.bv = BitVectors.EMPTY_VECTOR;
     }
 
     public int getData() {
